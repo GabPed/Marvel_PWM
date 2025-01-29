fe6a2b6fbb353793d7b9f7bcb26232d34e0a5702
GabPed <gabrielpedranzini@gmail.com>
2025-01-29 23:33:21 +0100

    Aggiunta swagger con file yaml

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,10 @@ import cors from 'cors'; //Ci permette a gestire le chiamate cross origin
 import mongoSanitize from 'express-mongo-sanitize' //Ci permette di evitare injection
 import dotenv from 'dotenv'
 
+import fs from 'fs';
+import yaml from 'js-yaml';
+import swaggerUi from "swagger-ui-express";
+
 import avatarsRoutes from './routes/avatars.js';
 import usersRoutes from './routes/users.js';
 import albumsRoutes from './routes/albums.js';
@@ -17,9 +21,9 @@ import './cronjob/characters.js';  // Percorso relativo
 import {authenticateToken} from './middleware/auth.js';
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT;
+
+dotenv.config();
 
 //app.use(mongoSanitize());
 app.use(cors()); //Si fa una sola volta
@@ -33,12 +37,19 @@ app.use('/characters',authenticateToken,charactersRoutes);
 app.use('/auth',registerRoutes);
 app.use('/avatars',avatarsRoutes);
 
-console.log(process.env.CONNECTION_URL)
+
+// Configurazione Swagger
+const swaggerDocument = yaml.load(fs.readFileSync('swagger.yaml', 'utf8'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 mongoose.connect(process.env.CONNECTION_URL)
 .then(() =>{
     
     app.listen(PORT, () => { //Ci mettiamo in ascolto solo se la connessione al DB è andata a buon fine
         console.log('Server running on port: '+PORT);
+        
     });
 })
 .catch(Error=>console.error(error))
